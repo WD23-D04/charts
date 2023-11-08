78fe8b8e8e9457d53596cfe4bf06ce91dd5285a6
AlbertLeipzig <albertclemvill@gmail.com>
2023-11-08 12:56:39 +0100

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -17,18 +17,36 @@ function App() {
       'Getränke',
     ],
     datasets: [
-      {
+      /* {
         label: 'Lagger',
         data: [86, 145, 293, 42, 189, 337, 127, 321, 66, 272, 248, 75],
         borderWidth: 1,
         backgroundColor: ['purple', 'orange', 'crimson', 'darkgreen', 'silver'],
       },
       {
-        label : "Preise",
+        label: 'Preise',
         data: [10, 20, 10, 30, 12, 17, 32, 28, 95, 125, 50, 70],
         borderWidth: 1,
         backgroundColor: ['purple', 'orange', 'crimson', 'darkgreen', 'silver'],
-      }
+      }, */
+      {
+        label: 'Products',
+        data: {
+          getreideprodukte: 86,
+          gemüse: 175,
+          obst: 145,
+          fleischUndGeflügel: 392,
+          fischUndMeeresfrüchte: 45,
+          milchprodukte: 45,
+          eier: 54,
+          hülsenfrüchte: 65,
+          nüsseUndSamen: 45,
+          ÖleUndFette: 78,
+          Süßigkeiten: 98,
+          Getränke: 78,
+        },
+        backgroundColor: ['purple', 'orange', 'crimson', 'darkgreen', 'silver'],
+      },
     ],
   };
 
