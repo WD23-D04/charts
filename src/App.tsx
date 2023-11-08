2f97bd0f6254dab99a4a42da15f3692466e32ba2
AlbertLeipzig <albertclemvill@gmail.com>
2023-11-08 10:09:12 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import ChartComponent from './components/ChartComponent';
+
+function App() {
+  const chartData = {
+    labels: [
+      'Getreideprodukte',
+      'Gemüse',
+      'Obst',
+      'Fleisch und Geflügel',
+      'Fisch und Meeresfrüchte',
+      'Milchprodukte',
+      'Eier',
+      'Hülsenfrüchte',
+      'Nüsse und Samen',
+      'Öle und Fette',
+      'Süßigkeiten',
+      'Getränke',
+    ],
+    datasets: [
+      {
+        label: 'Lagger',
+        data: [86, 145, 293, 42, 189, 337, 127, 321, 66, 272, 248, 75],
+        borderWidth: 1,
+        backgroundColor: ['purple', 'orange', 'crimson', 'darkgreen', 'silver'],
+      },
+      {
+        label : "Preise",
+        data: [10, 20, 10, 30, 12, 17, 32, 28, 95, 125, 50, 70],
+        borderWidth: 1,
+        backgroundColor: ['purple', 'orange', 'crimson', 'darkgreen', 'silver'],
+      }
+    ],
+  };
+
+  return (
+    <div>
+      <ChartComponent data={chartData} />{' '}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChartComponent.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useRef } from 'react';
+import Chart, {
+  ChartConfiguration,
+  ChartOptions,
+  ChartType,
+} from 'chart.js/auto';
+
+type ChartData = {
+  labels: string[];
+  datasets: {
+    label: string;
+    data: number[];
+    borderWidth: number;
+  }[];
+};
+
+type ChartProps = {
+  data: ChartData;
+};
+
+const chartOptions: ChartOptions = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+  plugins: {
+    title: {
+      display : true,
+      text : "Lebensmittel",
+      color: 'tomato',
+    },
+  },
+};
+
+const ChartComponent: React.FC<ChartProps> = ({ data }) => {
+  const chartRef = useRef<HTMLCanvasElement | null>(null);
+
+  useEffect(() => {
+    if (chartRef.current) {
+      const ctx = chartRef.current.getContext('2d');
+
+      if (ctx) {
+        const chartConfig: ChartConfiguration = {
+          type: 'bar' as ChartType,
+          data: data,
+          options: chartOptions,
+        };
+
+        const myChart = new Chart(ctx, chartConfig);
+
+        return () => {
+          myChart.destroy();
+        };
+      }
+    }
+  }, [data]);
+
+  return (
+    <div>
+      <canvas ref={chartRef}></canvas>
+    </div>
+  );
+};
+
+export default ChartComponent;
